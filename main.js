99c676c298f0baf4a64f8eafdb089ed99100ab13
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-06-05 14:18:40 +0300

    второй урок в мастере

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-let money, income, addExpenses, deposit,  mission, period;
-alert('hello');
-console.log('message');
+let money = 150000,
+    income = 'фриланс',
+    addExpenses = 'Интернет, Коммуналка, Продукты',
+    deposit = false,
+    mission = 10000000,
+    period = 12;
+
+
+console.log(typeof money);
+console.log(typeof money);
+console.log(typeof money);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + 'месяцев');
+console.log('Цель заработать ' + mission + 'долларов');
+
+addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+
+let budjetDay = money / 30;
+console.log(budjetDay);
+
+
+
+
+
+
+
 
