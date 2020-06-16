42d884d16a817aefaacb25fbe57cb0a3620faebb
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-06-16 12:51:46 +0300

    девятый урок

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const startButton = document.getElementById('start'),
+    incomeAddButton = document.getElementsByTagName('button')[0],
+    expensesAddButton = document.getElementsByTagName('button')[1],
+    depositCheck = document.querySelector('#deposit-check'),
+    additionalIncomeItems = document.querySelectorAll('.additional_income-item'),
+    budjetDayValue = document.getElementsByClassName('result-total')[1],
+    expensesMonthValue = document.getElementsByClassName('result-total')[2],
+    additionalIncomeValue = document.getElementsByClassName('result-total')[3],
+    additionalExpensesValue = document.getElementsByClassName('result-total')[4],
+    incomePeriodValue = document.getElementsByClassName('result-total')[5],
+    targetMonthValue = document.getElementsByClassName('result-total')[6],
+    salaryAmount = document.querySelector('.salary-amount'),
+    incomeTitle = document.querySelector('.income-title'),
+    incomeAmount = document.querySelector('.income-amount'),
+    expensesTitle = document.querySelector('.expenses-title'),
+    expensesAmount = document.querySelector('.expenses-amount'),
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+    targetAmount = document.querySelector('.target-amount'),
+    periodSelect = document.querySelector('.period-select');
+
+
+
+
+
+
+
+
+
+
